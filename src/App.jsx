2f2a17a462bf7ccd0f6eef62aafec48c444fe5f6
unknown <rupe.md@gmail.com>
2021-05-30 16:41:23 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Homepage from "./pages/Homepage";
 import NotFound from "./pages/NotFound";
 import News from "./pages/News";
 import Settings from "./pages/Settings";
-import Article from './components/Article';
+import Article from "./components/Article";
 import { Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
@@ -29,11 +29,13 @@ const App = () => {
               <Route exact path="/news" component={News} />
               <Route exact path="/news/:id" component={Article} />
               <Route exact path="/settings" component={Settings} />
-              <Route path="*" component={NotFound} />
+              <Redirect to="/" />
             </Switch>
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>Anton Musiienko 1 TKR © 2021</Footer>
+        <Footer style={{ textAlign: "center" }}>
+          Anton Musiienko 1 TKR © 2021
+        </Footer>
       </Layout>
     </Router>
   );
